a4e9b6b3e4be9238a2e1f68de27f4fa822ad0df9
wyk9787 <wangyika@grinnell.edu>
2019-09-18 15:01:36 -0700

    HW3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Garrett Wang Photography"
   end
 
+  def navibar_test
+    assert_select "a[href=?]", "/static_pages/home.html"
+    assert_select "a[href=?]", "/static_pages/home.html"
+    assert_select "a[href=?]", "/static_pages/about.html"
+    assert_select "a[href=?]", "/static_pages/help.html"
+  end
+    
   test "should get root" do
     get root_url
     assert_response :success
@@ -15,18 +22,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
+    navibar_test
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
+    navibar_test
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+    navibar_test
   end
 
 end
