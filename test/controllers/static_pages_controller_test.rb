9741e68d83eb27f6bededc2bf7fe24154e44fcbc
wyk9787 <wangyika@grinnell.edu>
2019-10-03 14:40:34 -0500

    add user signup controller and router

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,21 +18,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path 
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
     navibar_test
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path 
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
     navibar_test
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path 
     assert_response :success
     assert_select "title", "About | #{@base_title}"
     navibar_test
